bd6ee6ad42fce78db13a07e11d6dd10b64f5ec23
jihyunglee <jhlee7125@gmail.com>
2022-06-16 04:52:13 +0900

    creat header and decorate it with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import Home from './pages/home/Home';
 import './App.css';
 
 function App() {
   return (
     <>
       <TopBar />
+      <Home />
     </>
   );
 }
