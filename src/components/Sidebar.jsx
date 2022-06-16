c2b66b05ff17b467102015a3c56ac8b8a7748889
jihyunglee <jhlee7125@gmail.com>
2022-06-16 21:34:59 +0900

    create sidebar contents and styling with css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,39 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <>
-      <div className="sidebar">sidebar</div>
+      <div className="sidebar">
+        <div className="sidebarItem">
+          <span className="sidebarTitle">ABOUT ME</span>
+          <img
+            src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+            alt=""
+          />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+            aliquam dolores illum.
+          </p>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">Life</li>
+            <li className="sidebarListItem">Music</li>
+            <li className="sidebarListItem">Style</li>
+            <li className="sidebarListItem">Sport</li>
+            <li className="sidebarListItem">Tech</li>
+            <li className="sidebarListItem">Cinema</li>
+          </ul>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">FOLLOW US</span>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+            <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+            <i className="sidebarIcon fa-brands fa-pinterest-square"></i>
+            <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
