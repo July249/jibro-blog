2afa7194db198d44520c8ff32cb2451602120c50
jihyunglee <jhlee7125@gmail.com>
2022-06-17 00:37:42 +0900

    create settings component and styling with css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import TopBar from './components/TopBar';
 // import Homepage from './pages/homepage/Homepage';
 // import Single from './pages/single/Single';
-import Write from './pages/write/Write';
+// import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
 import './App.css';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Sidebar from '../../components/Sidebar';
+import './settings.css';
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              className=""
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-regular fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: 'none' }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Jihyung Lee" />
+          <label>Email</label>
+          <input type="email" placeholder="easyshiny724@gmail.com" />
+          <label>Password</label>
+          <input type="password" placeholder="" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Settings;
