cdd9a1c38cec39bdcd442343ab2b858c4f2e7369
jihyunglee <jhlee7125@gmail.com>
2022-06-16 23:26:52 +0900

    create SInglePost component and styling with css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import TopBar from './components/TopBar';
-import Homepage from './pages/homepage/Homepage';
+// import Homepage from './pages/homepage/Homepage';
+import Single from './pages/single/Single';
 import './App.css';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Homepage />
+      <Single />
     </>
   );
 }
---
+++ src/components/SinglePost.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const SinglePost = () => {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet modi.
+          <div className="singlePostEdit">
+            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-regular fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Jihyung Lee</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum
+          ipsum doloremque recusandae voluptatum saepe magni quisquam, fugit
+          doloribus nihil consequatur expedita. Alias aut expedita voluptatum
+          voluptate hic officiis totam. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Officiis eum ipsum doloremque recusandae voluptatum
+          saepe magni quisquam, fugit doloribus nihil consequatur expedita.
+          Alias aut expedita voluptatum voluptate hic officiis totam. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum ipsum
+          doloremque recusandae voluptatum saepe magni quisquam, fugit doloribus
+          nihil consequatur expedita. Alias aut expedita voluptatum voluptate
+          hic officiis totam. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Officiis eum ipsum doloremque recusandae voluptatum
+          saepe magni quisquam, fugit doloribus nihil consequatur expedita.
+          Alias aut expedita voluptatum voluptate hic officiis totam. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum ipsum
+          doloremque recusandae voluptatum saepe magni quisquam, fugit doloribus
+          nihil consequatur expedita. Alias aut expedita voluptatum voluptate
+          hic officiis totam. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Officiis eum ipsum doloremque recusandae voluptatum
+          saepe magni quisquam, fugit doloribus nihil consequatur expedita.
+          Alias aut expedita voluptatum voluptate hic officiis totam. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum ipsum
+          doloremque recusandae voluptatum saepe magni quisquam, fugit doloribus
+          nihil consequatur expedita. Alias aut expedita voluptatum voluptate
+          hic officiis totam. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Officiis eum ipsum doloremque recusandae voluptatum
+          saepe magni quisquam, fugit doloribus nihil consequatur expedita.
+          Alias aut expedita voluptatum voluptate hic officiis totam. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Officiis eum ipsum
+          doloremque recusandae voluptatum saepe magni quisquam, fugit doloribus
+          nihil consequatur expedita. Alias aut expedita voluptatum voluptate
+          hic officiis totam.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePost;
