c593a9db6c22ec5846d7a6da6ad278ad03574450
jihyunglee <jhlee7125@gmail.com>
2022-06-16 22:07:45 +0900

    create Post component and styling with css

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Post = ({ img }) => {
+  return (
+    <div className="post">
+      <img className="postImg" src={img} alt="" />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">Lorem ipsum dolor sit amet</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis veniam
+        sequi illo facilis, a nihil repudiandae iusto. Eos labore earum ea
+        maxime rerum voluptas. Corrupti unde fuga placeat iure hic! Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit. Omnis veniam sequi illo
+        facilis, a nihil repudiandae iusto. Eos labore earum ea maxime rerum
+        voluptas. Corrupti unde fuga placeat iure hic! Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Omnis veniam sequi illo facilis, a
+        nihil repudiandae iusto. Eos labore earum ea maxime rerum voluptas.
+        Corrupti unde fuga placeat iure hic!
+      </p>
+    </div>
+  );
+};
+
+export default Post;
