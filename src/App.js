08f79ba020f6e3c1a44647cbe15c71c4959fbfe7
jihyunglee <jhlee7125@gmail.com>
2022-06-16 23:55:19 +0900

    create write page component and styling with css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import TopBar from './components/TopBar';
 // import Homepage from './pages/homepage/Homepage';
-import Single from './pages/single/Single';
+// import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 import './App.css';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './write.css';
+
+const Write = () => {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: 'none' }} />
+          <input
+            className="writeInput"
+            type="text"
+            placeholder="Title"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
