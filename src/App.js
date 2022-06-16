25ebb3f52796237ec20b8299462894da4ba97573
jihyunglee <jhlee7125@gmail.com>
2022-06-17 01:35:10 +0900

    set react-router-dom and make structure of Route path and components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import TopBar from './components/TopBar';
-// import Homepage from './pages/homepage/Homepage';
-// import Single from './pages/single/Single';
-// import Write from './pages/write/Write';
+import Homepage from './pages/homepage/Homepage';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
