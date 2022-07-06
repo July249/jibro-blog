ecebce1264635c422636715341feea2fa56f65c0
jihyunglee <jhlee7125@gmail.com>
2022-07-06 21:32:45 +0900

    delete post

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import { Context } from '../context/Context';
 import axios from 'axios';
 
 const SinglePost = () => {
@@ -9,15 +10,24 @@ const SinglePost = () => {
   const [post, setPost] = useState({});
 
   const PF = 'http://localhost:5000/images/';
+  const { user } = useContext(Context);
 
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get('/posts/' + path);
-      // console.log(res);\
       setPost(res.data);
     };
     getPost();
   }, [path]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+        data: { username: user.username },
+      });
+      window.location.replace('/');
+    } catch (err) {}
+  };
 
   return (
     <div className="singlePost">
@@ -28,10 +38,15 @@ const SinglePost = () => {
 
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-            <i className="singlePostIcon fa-regular fa-trash-can"></i>
-          </div>
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+              <i
+                className="singlePostIcon fa-regular fa-trash-can"
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
