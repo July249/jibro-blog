e2d5d72b5e7c431646ef887dd1759a05a247afa2
jihyunglee <jhlee7125@gmail.com>
2022-07-12 20:24:01 +0900

    add About component and de-activate Register, Article component 'cause these component is not ready, yet

--- api/client/src/App.js
+++ api/client/src/App.js
@@ -2,9 +2,11 @@ import TopBar from './components/TopBar';
 import Homepage from './pages/homepage/Homepage';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
+// import Article from './pages/article/Article';
+import About from './pages/about/About';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
-import Register from './pages/register/Register';
+// import Register from './pages/register/Register';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useContext } from 'react';
@@ -17,11 +19,13 @@ function App() {
       <TopBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/register" element={user ? <Homepage /> : <Register />} />
+        {/* <Route path="/register" element={user ? <Homepage /> : <Register />} /> */}
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={user ? <Homepage /> : <Login />} />
         <Route path="/write" element={user ? <Write /> : <Login />} />
         <Route path="/settings" element={user ? <Settings /> : <Login />} />
         <Route path="/post/:postId" element={<Single />} />
+        {/* <Route path="/posts" element={<Article />} /> */}
       </Routes>
     </Router>
   );
