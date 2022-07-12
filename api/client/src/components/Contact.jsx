6a56e0b6a089ff4791379b17085bdd790a173eeb
jihyunglee <jhlee7125@gmail.com>
2022-07-12 21:59:56 +0900

    edit typo ("class" >> "className")

--- api/client/src/components/Contact.jsx
+++ api/client/src/components/Contact.jsx
@@ -3,9 +3,9 @@ import '../App.css';
 const Contact = () => {
   return (
     <footer className="contact">
-      <h1 class="contactTitle">Contact</h1>
-      <h2 class="contactEmail">easyshiny724@gmail.com</h2>
-      <h4 class="contactRights">2022 Jihyung Lee - All rights reserved</h4>
+      <h1 className="contactTitle">Contact</h1>
+      <h2 className="contactEmail">easyshiny724@gmail.com</h2>
+      <h4 className="contactRights">2022 Jihyung Lee - All rights reserved</h4>
     </footer>
   );
 };
