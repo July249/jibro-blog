d17ca7b31f6eae34680c827fbbccbfae4a205123
jihyunglee <jhlee7125@gmail.com>
2022-07-12 23:34:54 +0900

    de-activate the button to link Register component

--- api/client/src/pages/login/Login.jsx
+++ api/client/src/pages/login/Login.jsx
@@ -47,11 +47,11 @@ const Login = () => {
           Login
         </button>
       </form>
-      <button className="loginRegisterButton">
+      {/* <button className="loginRegisterButton">
         <Link className="link" to="/register">
           Register
         </Link>
-      </button>
+      </button> */}
     </div>
   );
 };
