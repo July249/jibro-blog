054f28d136756481f84c3ea3bd06a21c8b5d1806
jihyunglee <jhlee7125@gmail.com>
2022-12-07 00:11:02 +0900

    add: Home 컴포넌트에서의 banner-contents 콘텐츠

---
+++ src/components/Banner/contents/Mainbanner.js
@@ -0,0 +1,13 @@
+export const Mainbanner = () => {
+  return (
+    <div className="banner-contents">
+      <p className="sub-text">React Blog</p>
+      <p className="main-text">My BLOG</p>
+      <p className="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        nostrum dolor provident beatae deserunt magnam, illo neque. Maiores
+        ipsum commodi dolorum beatae aliquid laboriosam est
+      </p>
+    </div>
+  );
+};
