2c12804fa68d2aafee0d7a4459bcf29e5d00d6da
jihyunglee <jhlee7125@gmail.com>
2022-12-06 19:22:10 +0900

    add: posts 콘텐츠를 표기하는 기능 구현

---
+++ src/components/Main/Main.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import UserContext from '../../context/UserContext';
+import Posts from './posts/Posts';
+import About from './about/About';
+/* temporary style */
+import './main.css';
+
+export default function Main() {
+  const { isLogin } = useContext(UserContext);
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://github.com/July249/jibro-blog/blob/main/public/contents/posts.json'
+      )
+      .then((json) => {
+        // console.log(json)
+        // console.log(json.data)
+        setPosts(json.data);
+      });
+  }, []);
+
+  return (
+    <main>
+      {posts === undefined ? (
+        <></>
+      ) : (
+        <div className="max-width">
+          <h2 className="a11y-hidden">Post</h2>
+          <ul className="posts">
+            {/* components array로 생성 */}
+            <Posts posts={posts} />
+          </ul>
+          {isLogin ? <About /> : <></>}
+        </div>
+      )}
+    </main>
+  );
+}
